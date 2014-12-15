a0d992b7345bef600ddc945b431d580b705c2bc3
Hesam Samimi <hmsamimi@yahoo.com>
2014-12-15 15:42:39 -0800

    3d chain demo

---
+++ 3d/demo/example-chain.js
@@ -0,0 +1,21 @@
+examples['chain']= function() {
+    rc.setOption('renderMode', 1)    
+    var points = [], count = 9
+    var x = -200
+    var p1 = new Point3D(x, 0, 0)
+    var s1 = rc.add(new Sphere(p1)), p2 = undefined, s2 = undefined
+    for (var i = 0; i < count; i++) {
+	x += 50
+	p2 = new Point3D(x, 0, 0)
+	s2 = rc.add(new Sphere(p2))
+	var l1 = rc.add(new Cylinder(p1, p2))
+	rc.addConstraint(Sketchpad.geom3d.LengthConstraint, p1, p2, 50)
+	points.push(p1)
+	if (i < 8)
+	    p1 = p2
+    }
+    rc.addConstraint(Sketchpad.geom3d.MotorConstraint, points[0], points[1], 1)
+    rc.addConstraint(Sketchpad.geom3d.MotorConstraint, points[count-2], points[count-1], 1)
+    rc.addConstraint(Sketchpad.simulation.TimerConstraint, rc.add(new Timer3D(10)))
+};
+
