862ffd89c359a1fdfd281d2bc52a19143cdc354b
Hesam Samimi <hmsamimi@yahoo.com>
2015-02-06 15:10:02 -0800

    minor codemirror up

--- 2d/demo/example-cdg.js
+++ 2d/demo/example-cdg.js
@@ -1,5 +1,5 @@
 examples.cdg = function() {
-    var fonts = ['Avenir', 'Lucida Sans']//['Arial Rounded MT Bold', 'Gill Sans',  'Lucida Sans Typewriter',  'serif', 'sans-serif', 'Garamond', 'Arial', 'Arial Black', 'Arial Narrow', 'Avant Garde', 'Calibri', 'Candara', 'Century Gothic', 'Franklin Gothic Medium', 'Futura', 'Geneva', 'Helvetica', 'Impact', 'Lucida Grande', 'Optima', 'Segoe UI', 'Tahoma', 'Trebuchet MS', 'Verdana', 'Big Caslon', 'Bodoni MT', 'Book Antiqua', 'Calisto MT', 'Cambria', 'Didot', 'Georgia', 'Goudy Old Style', 'Hoefler Text', 'Lucida Bright', 'Palatino', 'Perpetua', 'Rockwell', 'Rockwell Extra Bold', 'Baskerville', 'Times New Roman', 'Consolas', 'Courier New', 'Lucida Console', 'Monaco', 'Andale Mono', 'Copperplate', 'Papyrus', 'cursive', 'monospace', 'serif', 'sans-serif', 'fantasy', 'Avenir', 'Bookman Old Style', 'Bradley Hand ITC', 'Century', 'Century Gothic', 'Comic Sans MS', 'Courier', 'Courier New', 'Georgia', 'Gentium', 'Impact', 'King', 'Lucida Console', 'Lalit', 'Modena', 'Monotype Corsiva', 'Papyrus', 'Tahoma', 'TeX', 'Times', 'Trebuchet MS',  'Verona']
+    var fonts = ['Avenir']//, 'Lucida Sans']//['Arial Rounded MT Bold', 'Gill Sans',  'Lucida Sans Typewriter',  'serif', 'sans-serif', 'Garamond', 'Arial', 'Arial Black', 'Arial Narrow', 'Avant Garde', 'Calibri', 'Candara', 'Century Gothic', 'Franklin Gothic Medium', 'Futura', 'Geneva', 'Helvetica', 'Impact', 'Lucida Grande', 'Optima', 'Segoe UI', 'Tahoma', 'Trebuchet MS', 'Verdana', 'Big Caslon', 'Bodoni MT', 'Book Antiqua', 'Calisto MT', 'Cambria', 'Didot', 'Georgia', 'Goudy Old Style', 'Hoefler Text', 'Lucida Bright', 'Palatino', 'Perpetua', 'Rockwell', 'Rockwell Extra Bold', 'Baskerville', 'Times New Roman', 'Consolas', 'Courier New', 'Lucida Console', 'Monaco', 'Andale Mono', 'Copperplate', 'Papyrus', 'cursive', 'monospace', 'serif', 'sans-serif', 'fantasy', 'Avenir', 'Bookman Old Style', 'Bradley Hand ITC', 'Century', 'Century Gothic', 'Comic Sans MS', 'Courier', 'Courier New', 'Georgia', 'Gentium', 'Impact', 'King', 'Lucida Console', 'Lalit', 'Modena', 'Monotype Corsiva', 'Papyrus', 'Tahoma', 'TeX', 'Times', 'Trebuchet MS',  'Verona']
     var colors = ['black', 'purple', 'blue', '#cc3d3d', 'gray', '#4380e1', 'green'] 
     var radius = 45, thickness = 15, width = 110, fontColor = 'white', opacity = 0.85, fontSize = 60, colorIdx = -1
     fonts.forEach(function(font) {	
@@ -7,11 +7,11 @@ examples.cdg = function() {
 	var position = rc.getRandomPoint(), centerX = position.x, centerY = position.y,  color = colors[colorIdx]
 	var p1 = rc.add(new Point(centerX, centerY, color, radius, undefined, opacity, fontColor, fontSize, font))
 	var p2 = rc.add(new Point(centerX + width, centerY, color, radius, undefined, opacity, fontColor, fontSize, font))
-	//var p3 = rc.add(new Point(centerX + width, centerY + width, color, radius, undefined, opacity, fontColor, fontSize, font))
-	var p3 = rc.add(new Point(centerX + width, centerY + width, color, radius, undefined, opacity, fontColor, fontSize / 4, font))
+	var p3 = rc.add(new Point(centerX + width, centerY + width, color, radius, undefined, opacity, fontColor, fontSize, font))
+	//var p3 = rc.add(new Point(centerX + width, centerY + width, color, radius, undefined, opacity, fontColor, fontSize / 4, font))
 	p1._selectionIndices.push('C')
 	p2._selectionIndices.push('D')
-	p3._selectionIndices.push(font)//'G')
+	p3._selectionIndices.push('G')//font)
 	rc.add(new Line(p1, p2, color, thickness, undefined, opacity))
 	rc.add(new Line(p2, p3, color, thickness, undefined, opacity))
 	
