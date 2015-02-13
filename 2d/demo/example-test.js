0d84c050f893f92570af6a852b05c747da77ea45
Hesam Samimi <hmsamimi@yahoo.com>
2015-02-12 17:23:33 -0800

    minor update to allow not initializing

--- 2d/demo/example-test.js
+++ 2d/demo/example-test.js
@@ -1,13 +1,17 @@
 examples.test = function() {
-    sketchpad.solveEvenWithoutErrorOnPriorityDifferences = true
-    var p1 = rc.add(new Point(200, 200))
-    var p2 = rc.add(new Point(300, 200))
-    
-    rc.add(new Line(p1, p2))
+    rc.setOption('renderMode', 2)
+    var center = {x: 700, y: 500}
+    var py = new Point(center.x, center.y - 500)
+    var px = new Point(center.x + 500, center.y)
+    var po = new Point(center.x, center.y)
+    var p1 = rc.add(new Point(center.x + 100, center.y - 100))
+    var p2 = rc.add(new Point(center.x + 200, center.y - 200))
+    rc.add(new Line(po, py))
+    rc.add(new Line(po, px))
     var c1 = new Sketchpad.geom.LengthConstraint(p1, p2, 100)
-    var c2 = new Sketchpad.geom.LengthConstraint(p1, p2, 200)
-    c2.__priority = 2
+    var c2 = new Sketchpad.geom.CoordinateConstraint(p1, center.x, center.y)
     rc.addNewConstraint(c1)
     rc.addNewConstraint(c2)
+
 };
 
